8878c51844628260501cb1980c9f8b0e67903f5b
Robson Duarte <robosn.o.d@gmail.com>
2023-04-15 15:07:29 -0300

    inital project

---
+++ src/test/java/com/uvocab/api/UVocabApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.uvocab.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UVocabApiApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
