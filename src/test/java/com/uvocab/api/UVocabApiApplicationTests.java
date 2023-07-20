be5b9db2b864499f14d33f7da625b1c2213f53ae
Robson Duarte <robosn.o.d@gmail.com>
2023-07-20 14:28:13 -0300

    create application test profile to use the docker compose plugin as localhost

--- src/test/java/com/uvocab/api/UVocabApiApplicationTests.java
+++ src/test/java/com/uvocab/api/UVocabApiApplicationTests.java
@@ -3,8 +3,10 @@ package com.uvocab.api;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UVocabApiApplicationTests {
 	@Test
 	void contextLoads() {
