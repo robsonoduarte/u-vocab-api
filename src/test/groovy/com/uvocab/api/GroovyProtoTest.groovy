da1d64a3da12075be611bda5c4b964bbbe7295a3
Danilosimoes <danilossimoes28@gmail.com>
2023-09-12 20:03:34 -0300

    New config for proto

---
+++ src/test/groovy/com/uvocab/api/GroovyProtoTest.groovy
@@ -0,0 +1,14 @@
+package com.uvocab.api
+
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+class GroovyProtoTest {
+	@Test
+
+	void test() {
+		Test
+		assertEquals(1, 1);
+	}
+}
