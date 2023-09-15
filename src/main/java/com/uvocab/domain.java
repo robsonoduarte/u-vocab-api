673ff5b53a3486d94d5b9fd96fdc5be1a2708ba4
Danilosimoes <danilossimoes28@gmail.com>
2023-09-15 14:39:04 -0300

    :pray:

---
+++ src/main/java/com/uvocab/domain.java
@@ -0,0 +1,14 @@
+package com.uvocab;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@Data
+public class domain {
+  private String test;
+}
