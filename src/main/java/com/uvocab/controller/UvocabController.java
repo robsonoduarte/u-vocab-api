36f86f485ca5ae784aac36afc2d0f311506f42ce
Danilosimoes <danilossimoes28@gmail.com>
2023-10-04 14:36:07 -0300

    spotless correction

--- src/main/java/com/uvocab/controller/UvocabController.java
+++ src/main/java/com/uvocab/controller/UvocabController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
 @Controller
 @Slf4j
 @RequestMapping("/uvocab")
