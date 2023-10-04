b7f56577dc30fc82401c47c2d1c58e00a85c4407
Danilosimoes <danilossimoes28@gmail.com>
2023-10-04 14:32:34 -0300

    get-message-test

---
+++ src/main/java/com/uvocab/controller/UvocabController.java
@@ -0,0 +1,16 @@
+package com.uvocab.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@Slf4j
+@RequestMapping("/uvocab")
+public class UvocabController {
+  @GetMapping
+  public String testMessage() {
+    return "OK";
+  }
+}
