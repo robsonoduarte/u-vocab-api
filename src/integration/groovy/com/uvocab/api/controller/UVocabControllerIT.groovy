3230382c343b4dbafe536e06ed252bb17f3c9db8
Danilosimoes <danilossimoes28@gmail.com>
2023-10-27 00:29:32 -0300

    :pray:

--- src/integration/groovy/com/uvocab/api/controller/UVocabControllerIT.groovy
+++ src/integration/groovy/com/uvocab/api/controller/UVocabControllerIT.groovy
@@ -1,5 +1,12 @@
 package com.uvocab.api.controller
 
+import static org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 
 class UVocabControllerIT  {
+	@Test
+	void Test () {
+		assertEquals(1, 2);
+	}
 }
