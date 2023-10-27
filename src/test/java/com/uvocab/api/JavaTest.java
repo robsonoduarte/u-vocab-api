0b0c5b1982bef7637003b6a4f4b3334edf06e0a2
robsonoduarte <robson.o.d@gmail.com>
2023-10-27 15:06:05 -0300

    robson patch

---
+++ src/test/java/com/uvocab/api/JavaTest.java
@@ -0,0 +1,12 @@
+package com.uvocab.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class JavaTest {
+  @Test
+  void test() {
+    assertEquals(1, 1);
+  }
+}
