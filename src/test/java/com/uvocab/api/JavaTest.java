119dcb0cdf33b31d137b6816dac1fc7e8c234c99
Danilo Simões <63322446+Danilosimoes@users.noreply.github.com>
2023-10-31 19:12:44 -0300

    separate integration and unit tests (#34)
    Co-authored-by: robsonoduarte <robson.o.d@gmail.com>

---
+++ src/test/java/com/uvocab/api/JavaTest.java
@@ -0,0 +1,12 @@
+package com.uvocab.api;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class JavaTest {
+  @Test
+  void test() {
+    assertTrue(true);
+  }
+}
