5589bba9bcf5eed65d87175eb836458a64dae539
Danilosimoes <danilossimoes28@gmail.com>
2023-11-01 23:32:41 -0300

    include post method

---
+++ src/main/java/com/uvocab/api/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.uvocab.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uvocab.protobuf.v1.User;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+
+  @PostMapping
+  public ResponseEntity<String> postUser(@RequestBody User user) {
+
+    return ResponseEntity.ok("");
+  }
+}
