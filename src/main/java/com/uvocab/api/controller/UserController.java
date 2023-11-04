bd1e728cde1dc0a8a3fa6abd2269cd8e5cf8d86d
Danilosimoes <danilossimoes28@gmail.com>
2023-11-04 12:06:27 -0300

    I need a help

--- src/main/java/com/uvocab/api/controller/UserController.java
+++ src/main/java/com/uvocab/api/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.uvocab.api.controller;
 
+import com.uvocab.api.repository.UVocabRepository;
+import com.uvocab.api.request.UVocabPostUser;
+import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.control.MappingControl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +14,14 @@ import uvocab.protobuf.v1.User;
 
 @RestController
 @RequestMapping("/v1/user")
+@Slf4j
 public class UserController {
 
+  @Autowired private UVocabRepository uVocabRepository;
+
   @PostMapping
-  public ResponseEntity<String> postUser(@RequestBody User user) {
-    return ResponseEntity.ok("");
+  public User postUser(UVocabPostUser uVocabPostUser) {
+    User user = User.
+    return uVocabRepository.save(user);
   }
 }
