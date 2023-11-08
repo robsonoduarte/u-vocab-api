7eb46f32fcb82668ec7d4cd1de994489e3cb74ec
robsonoduarte <robson.o.d@gmail.com>
2023-11-08 01:01:52 -0300

    :eyes:

--- src/test/java/com/uvocab/api/controller/UVocabControllerTest.java
+++ src/test/java/com/uvocab/api/controller/UVocabControllerTest.java
@@ -2,13 +2,11 @@ package com.uvocab.api.controller;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.Test;
-
 public class UVocabControllerTest {
 
   private final UVocabController uVocabController = new UVocabController();
 
-  //@Test
+  // @Test
   void shouldReturnWordVocabIsNotNull() {
     var word = uVocabController.getVocab();
     assertNotNull(word);
