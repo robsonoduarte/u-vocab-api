246e5d8f116509c4cff219fbe6f39da4fc20d164
Danilosimoes <danilossimoes28@gmail.com>
2024-01-19 15:38:20 -0300

    Help-me

--- src/main/java/com/uvocab/api/controller/VocabularyController.java
+++ src/main/java/com/uvocab/api/controller/VocabularyController.java
@@ -1,13 +1,16 @@
 package com.uvocab.api.controller;
 
+import com.uvocab.api.repository.VocabularyRepository;
 import com.uvocab.api.service.VocabularyService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uvocab.protobuf.v1.Vocabulary;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/word")
@@ -15,8 +18,16 @@ import uvocab.protobuf.v1.Vocabulary;
 public class VocabularyController {
   private final VocabularyService vocabularyService;
 
+
   @PostMapping
   public ResponseEntity<Vocabulary> postWord(@RequestBody Vocabulary word) {
     return ResponseEntity.ok(vocabularyService.save(word));
+  }
+  @GetMapping
+  public Page<Vocabulary> listVocab(@RequestParam int page,
+                                    @RequestParam int size){
+
+
+    return ResponseEntity.ok(vocabularyService.getAllWords(page, size));
   }
 }
--- src/main/java/com/uvocab/api/service/VocabularyService.java
+++ src/main/java/com/uvocab/api/service/VocabularyService.java
@@ -1,10 +1,18 @@
 package com.uvocab.api.service;
 
+import com.uvocab.api.exception.NotFoundException;
 import com.uvocab.api.mapper.VocabularyMapper;
 import com.uvocab.api.repository.VocabularyRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import uvocab.protobuf.v1.SearchRequest;
 import uvocab.protobuf.v1.Vocabulary;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -14,5 +22,17 @@ public class VocabularyService {
 
   public Vocabulary save(Vocabulary word) {
     return vocabularyMapper.toProto(vocabularyRepository.save(vocabularyMapper.toDomain(word)));
+  }
+
+  public List<com.uvocab.api.domain.Vocabulary> getAllWords(int pageNum, int pageSize){
+    Pageable pagination = PageRequest.of(pageNum, pageSize);
+
+    Page<com.uvocab.api.domain.Vocabulary> resultPage = vocabularyRepository.findAll(pagination);
+
+    if(resultPage.hasContent()){
+      return resultPage.getContent();
+    } else {
+      throw new NotFoundException("Not Found");
+    }
   }
 }
