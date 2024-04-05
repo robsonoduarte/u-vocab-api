e54ba045c24edca8cf905e3511b95351abe057b7
robsonoduarte <robson.o.d@gmail.com>
2024-04-05 20:13:48 -0300

    add auth security configuration with permit all request

---
+++ src/main/java/com/uvocab/api/auth/AuthSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.uvocab.api.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthSecurityConfiguration {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
+        .build();
+  }
+}
