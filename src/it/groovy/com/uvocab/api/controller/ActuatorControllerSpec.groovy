b24007dd94246305c07dc8ed749919f1884eab6c
robsonoduarte <robson.o.d@gmail.com>
2024-04-05 21:41:56 -0300

    add actuator to validade config

---
+++ src/it/groovy/com/uvocab/api/controller/ActuatorControllerSpec.groovy
@@ -0,0 +1,26 @@
+package com.uvocab.api.controller
+
+import com.uvocab.api.domain.Vocab
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Specification
+
+import static org.springframework.http.HttpMethod.GET
+
+
+@ActiveProfiles(['test'])
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ActuatorControllerSpec extends Specification{
+
+	@Autowired
+	TestRestTemplate restTemplate
+
+	def "should return the vocab with word ok"(){
+		def response = restTemplate.exchange('/actuator/health', GET, null, String)
+		expect:
+		response.statusCode == HttpStatus.OK
+	}
+}
