ec401e3c4ecdc388f5bc9b66d17fd99a6108a290
robsonoduarte <robson.o.d@gmail.com>
2024-04-06 00:10:21 -0300

    move revision

--- src/main/java/com/uvocab/api/auth/AuthUserDetails.java
+++ src/main/java/com/uvocab/api/auth/AuthUserDetails.java
@@ -8,9 +8,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @AllArgsConstructor
 public class AuthUserDetails implements UserDetailsService {
 
