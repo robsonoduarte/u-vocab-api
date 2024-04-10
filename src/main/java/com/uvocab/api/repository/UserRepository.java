6e994f3e9e0508a781805c160f852224570f0b20
Danilo Simões <63322446+Danilosimoes@users.noreply.github.com>
2024-04-09 23:01:58 -0300

    auth find by login (#56)
    * authuser: find by login
    
    * auth security configuration with permit all request (#57)
    
    * add auth security configuration with permit all request
    
    * add actuator to validade config
    
    * fix the text name
    
    * exclude the auth sec in sonar
    
    * remove the actuator
    
    * try again exclude the gradle
    
    * try again exclude the gradle again
    
    * try again exclude the gradle again
    
    * some test to coverage problem
    
    * fix the problem of coverage
    
    * fix the problem of coverage
    
    * more improve code
    
    * move revision
    
    * move revision
    
    * move revision
    
    * move revision
    
    * improve sonar quality
    
    ---------
    
    Co-authored-by: Robson Duarte <robson.o.d@gmail.com>

--- src/main/java/com/uvocab/api/repository/UserRepository.java
+++ src/main/java/com/uvocab/api/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.uvocab.api.repository;
 
 import com.uvocab.api.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByLogin(String login);
+}
