6957fa3ef0d445623de4ec7ad0da1157b91697a6
robsonoduarte <robson.o.d@gmail.com>
2024-06-04 21:53:30 -0300

    temp

--- src/main/java/com/uvocab/api/auth/AuthSecurityConfiguration.java
+++ src/main/java/com/uvocab/api/auth/AuthSecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AuthSecurityConfiguration {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http.authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll()).build();
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
+        .build();
   }
 }
