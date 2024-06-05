bfe1abd8f23dca6f92c1ab557b7912baaeda8a1d
robsonoduarte <robson.o.d@gmail.com>
2024-06-04 21:41:33 -0300

    fix spotlees

--- src/main/java/com/uvocab/api/auth/AuthSecurityConfiguration.java
+++ src/main/java/com/uvocab/api/auth/AuthSecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AuthSecurityConfiguration {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http
-        .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
-        .build();
+    return http.authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll()).build();
   }
 }
